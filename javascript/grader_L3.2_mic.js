f9592d4c000e7020d0f8160c9243f03a807276a4
Jay Luther <49818405+jay-ml@users.noreply.github.com>
2022-08-09 08:56:08 -0600

    Update grader_L3.2_mic.js

--- javascript/grader_L3.2_mic.js
+++ javascript/grader_L3.2_mic.js
@@ -42,7 +42,7 @@ function execute_student_code() {
 
     if (error) {
         //window.location.assign("/sensor-immersion-autograder/html/error.html");
-        console.log("Error : Staying Here");
+        console.log("Error : Staying");
     } else if (pass) {
         window.location.assign("/sensor-immersion-autograder/html/correct.html");
     } else if (!neopixelInit) {

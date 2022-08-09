99c747a610495c38b5dd80d9f273138ebcb9f460
Jay Luther <49818405+jay-ml@users.noreply.github.com>
2022-08-09 09:55:38 -0600

    Update grader_L3.2_mic.js

--- javascript/grader_L3.2_mic.js
+++ javascript/grader_L3.2_mic.js
@@ -42,7 +42,7 @@ function execute_student_code() {
 
     if (error) {
         //window.location.assign("/sensor-immersion-autograder/html/error.html");
-        console.log("Error : Staying Here");
+        console.log("Error : Staying");
     } else if (pass) {
         window.location.assign("/sensor-immersion-autograder/html/correct.html");
     } else if (!neopixelInit) {
@@ -84,6 +84,7 @@ class music extends MusicDefault {
 class neopixel extends NeopixelDefault {
 
     static create(pin, leds, mode){
+        super();
         neopixelInit = true;
         if (pin != DigitalPin.P12 || leds != 5){
             wrongPins = true;
